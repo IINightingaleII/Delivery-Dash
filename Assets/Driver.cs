53a5a14c4924697399a7a3d871bbaa4567b0ff97
Higen Putra <apasabun3@gmail.com>
2025-12-02 04:08:29 +0700

    intro

---
+++ Assets/Driver.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class Driver : MonoBehaviour
+{
+    [SerializeField]float steerSpeed = 0.5f;
+    [SerializeField]float moveSpeed = 0.05f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, 0, steerSpeed);
+        //transform.Translate(0, 0, 1);
+        transform.Translate(0,moveSpeed,0);
+    }
+}
