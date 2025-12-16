8232a15db3481b15cfee3bd65a95ceee0cc6798a
karinaqilanainggolan <162658060+karinaqilanainggolan@users.noreply.github.com>
2025-12-16 08:32:40 +0700

    15-22

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -4,8 +4,8 @@ using UnityEngine.InputSystem;
 
 public class Driver : MonoBehaviour
 {
-    [SerializeField]float steerSpeed = 180f;
-    [SerializeField]float moveSpeed = 10f;
+    [SerializeField] float steerSpeed = 180f;
+    [SerializeField] float moveSpeed = 10f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
     // Update is called once per frame
@@ -14,13 +14,13 @@ public class Driver : MonoBehaviour
         float steer = 0f;
         float move = 0f;
 
-        if(Keyboard.current.wKey.isPressed)
+        if (Keyboard.current.wKey.isPressed)
         {
             move = 1f;
             Debug.Log("W key is pressed");
         }
 
-        else if(Keyboard.current.sKey.isPressed)
+        else if (Keyboard.current.sKey.isPressed)
         {
             move = -1f;
             Debug.Log("S key is pressed");
@@ -31,17 +31,17 @@ public class Driver : MonoBehaviour
             steer = 1f;
             Debug.Log("A key is pressed");
         }
-        
-        else if(Keyboard.current.dKey.isPressed)
+
+        else if (Keyboard.current.dKey.isPressed)
         {
             steer = -1f;
             Debug.Log("D key is pressed");
         }
-        
+
         float moveamount = move * moveSpeed * Time.deltaTime;
         float steeramount = steer * steerSpeed * Time.deltaTime;
 
-        transform.Translate(0,moveamount,0);
+        transform.Translate(0, moveamount, 0);
         transform.Rotate(0, 0, steeramount);
         //transform.Translate(0, 0, 1);
     }
